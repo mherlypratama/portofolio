2d81c40abb15a96dd769a7baf6ff74549c7efa3b
M. Herly Pratama <tamaherly@gmail.com>
2025-12-25 22:32:38 +0700

    update

--- portofolio-app.py
+++ portofolio-app.py
@@ -7,25 +7,35 @@ st.set_page_config(
     initial_sidebar_state="expanded",  # Bisa 'auto', 'expanded', 'collapsed'
 )
 
-tab1, tab2, tab3 = st.tabs(["Dashboard", "Prediksi", "Settings"])
+tab1, tab2, tab3 = st.tabs(["Dashboard", "Project", "About Me"])
 
 with tab1:
-    st.header("Dashboard Utama")
-    st.write("Ini adalah tampilan ringkasan data.")
+    with st.container():
+        col1, col2 = st.columns(2)
+        with col1:
+            st.subheader("Hi!")
+            st.header("I'm M. Herly Pratama")
+            st.subheader("a Data Scientist and Data Analyst")
+        with col2:
+            st.image("profile_pic.png")
 
 with tab2:
-    st.header("Halaman Prediksi")
-    st.number_input("Masukkan nilai untuk prediksi:")
+    st.header("My Project")
+    with st.container():
+        st.markdown("---")
+        col1, col2, col3 = st.columns(3)
+        with col1:
+            st.image("churn analysis.png")
+            st.subheader("AI Churn Analysis")
+            st.markdown(
+                "AI Churn Analysis is a predictive analytics technique that uses machine learning algorithms to identify customers who are likely to stop doing business with a company, also known as customer churn. By analyzing historical data and behavior patterns, AI models can detect early warning signs of churn, such as changes in usage, engagement, or payment behavior. This enables businesses to proactively intervene with targeted retention strategies, such as personalized offers or support, to reduce churn rates and improve customer loyalty."
+            )
+            
+
 
 with tab3:
-    st.header("Pengaturan Aplikasi")
+    st.header("About Me")
     st.checkbox("Aktifkan mode gelap")
 
 
-st.subheader("Hi!")
-st.header("I'm M. Herly Pratama")
-st.subheader("a Data Scientist and Data Analyst")
-
-st.sidebar.header("Filter & Navigasi")
-st.sidebar.slider("Pilih nilai:", 0, 100, 50)
 # Konten aplikasi lainnya
