c5d0f9cd228ab3732845b29c49f7f639a994b57c
M. Herly Pratama <tamaherly@gmail.com>
2025-12-25 11:00:05 +0700

    rename

--- portofolio-app.py
+++ portofolio-app.py
@@ -10,7 +10,7 @@ st.set_page_config(
 # PATH GAMBAR
 # ===============================
 BASE_DIR = Path(__file__).resolve().parent
-IMAGE_PATH = BASE_DIR / "profil_pic.jpg"
+IMAGE_PATH = BASE_DIR / "profile_pic.png"
 
 # ===============================
 # CSS
