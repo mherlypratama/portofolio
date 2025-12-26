ad5ba1bdd4b43fff00386c79de0cd165afc1ea9d
M. Herly Pratama <tamaherly@gmail.com>
2025-12-26 17:32:10 +0700

    update

--- project1.py
+++ project1.py
@@ -1,9 +1,24 @@
 import streamlit as st
 from router import pindah_halaman
+import pandas as pd
 
 
 def halaman_project1():
     st.header("AI Churn Analysis")
+    st.markdown("---")
+
+    st.markdown(
+        """
+    Customer churn analysis is a predictive analytics technique that identifies customers who are likely to stop doing business with a company. Churn rate refers to the percentage of customers who discontinue a service or product over a certain period. This analysis uses historical customer data, such as purchase behavior, interactions with the company, and demographic characteristics, to detect patterns and factors influencing churn decisions. Machine learning algorithms predict which customers are likely to churn soon. The goal is to help companies identify high-risk customers and take proactive retention strategies, improving customer loyalty, reducing acquisition costs, and boosting revenue.
+
+    Examples include:
+    - Identifying at-risk customers and sending targeted offers
+    - Optimizing marketing strategies for retention
+    - Addressing factors driving churn
+
+    By leveraging churn analysis, companies make data-driven decisions to enhance customer retention and revenue growth.
+    """
+    )
 
     st.markdown("---")
     if st.button("Kembali"):
